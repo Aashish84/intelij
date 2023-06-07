fdad87a35123ec5f7ebbc433544b896d9e2e2a38
Aashish84 <aashishkr.thapa84@gmail.com>
2023-06-07 17:15:09 +0545

    basic_auth_without_jwt_EnabelMethodSecurity

--- revised_01/jwt_revised_01/src/main/java/com/example/jwt_revised_01/config/SecurityConfig.java
+++ revised_01/jwt_revised_01/src/main/java/com/example/jwt_revised_01/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @AllArgsConstructor
 public class SecurityConfig {
     private final CustomUserDetailService customUserDetailService;
